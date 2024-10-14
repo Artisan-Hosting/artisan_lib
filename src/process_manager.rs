9a784703f3b0016038a55bd2ed83a889b72bf3e0
Darrion Whitfield <dwhitfield@ramfield.net>
2024-10-14 00:10:42 -0500

    FIX: New process manager logic based on the runner

--- src/process_manager.rs
+++ src/process_manager.rs
@@ -1,21 +1,27 @@
 use libc::{c_int, kill, SIGKILL, SIGTERM};
 use nix::sys::wait::waitpid;
 use nix::unistd::Pid;
-use procfs::process::Process;
-use std::process::Stdio;
 use std::time::{Duration, Instant};
 use std::{io, thread};
-use tokio::process::Child;
-use tokio::process::Command;
+use tokio::process::{Child, Command};
+use std::process::Stdio;
+
+use crate::{common::{log_error, update_state}, log, logger::LogLevel, state_persistence::AppState};
+use dusa_collection_utils::{errors::ErrorArrayItem, types::PathType};
 
 pub struct ProcessManager;
 
 impl ProcessManager {
-    pub fn spawn_process(
+    /// Spawn an asynchronous process, similar to the `create_child` logic
+    pub async fn spawn_process(
         command: &str,
         args: &[&str],
         capture_output: bool,
+        state: &mut AppState,
+        state_path: &PathType,
     ) -> Result<Child, io::Error> {
+        log!(LogLevel::Trace, "Spawning process: {} with args: {:?}", command, args);
+
         let mut cmd = Command::new(command);
         cmd.args(args);
 
@@ -27,11 +33,32 @@ impl ProcessManager {
             cmd.stderr(Stdio::null());
         }
 
-        let child = cmd.spawn()?;
-        Ok(child)
+        // Spawn child process
+        let child = match cmd.spawn() {
+            Ok(child_process) => {
+                log!(LogLevel::Trace, "Child process spawned successfully: {:?}", child_process);
+                state.data = String::from("Process spawned");
+                state.event_counter += 1;
+                update_state(state, state_path);
+                Ok(child_process)
+            }
+            Err(e) => {
+                let err_ref = e.get_ref().unwrap();
+                
+                log!(LogLevel::Error, "Failed to spawn child process: {}", err_ref);
+                let error_item: ErrorArrayItem = ErrorArrayItem::new(dusa_collection_utils::errors::Errors::InputOutput, err_ref.to_string());
+                log_error(state, error_item, state_path);
+                Err(e)
+            }
+        };
+
+        child
     }
 
-    pub fn kill_process(pid: c_int) -> Result<(), Box<dyn std::error::Error>> {
+    /// Kill a process with SIGTERM, followed by SIGKILL if the process doesn't exit
+    pub async fn kill_process(pid: c_int, state: &mut AppState, state_path: &PathType) -> Result<(), Box<dyn std::error::Error>> {
+        log!(LogLevel::Trace, "Attempting to gracefully terminate process with PID: {}", pid);
+
         // Attempt to gracefully terminate first (send SIGTERM)
         unsafe {
             if kill(pid, SIGTERM) != 0 {
@@ -45,6 +72,7 @@ impl ProcessManager {
 
         // If still running, force kill the process (send SIGKILL)
         if Self::is_process_running(pid) {
+            log!(LogLevel::Warn, "Process with PID: {} did not terminate, sending SIGKILL", pid);
             unsafe {
                 if kill(pid, SIGKILL) != 0 {
                     return Err(io::Error::last_os_error().into());
@@ -53,87 +81,25 @@ impl ProcessManager {
             }
         }
 
+        log!(LogLevel::Trace, "Process with PID: {} terminated", pid);
+        state.data = String::from("Process terminated");
+        state.event_counter += 1;
+        update_state(state, state_path);
+
         Ok(())
     }
 
     /// Check if a process is running based on its PID
     pub fn is_process_running(pid: c_int) -> bool {
         unsafe {
-            // Sending signal 0 to a process is a way to check if it exists
-            // If the process exists and we have permission, kill(pid, 0) will return 0
-            // Otherwise, it will return -1 and set errno
             kill(pid, 0) == 0
         }
     }
 
-    pub fn is_process_blocking(pid: i32) -> Result<bool, Box<dyn std::error::Error>> {
-        let process = Process::new(pid)?;
-        let state = process.stat()?.state();
-        match state {
-            Ok(procfs::process::ProcState::Running) => Ok(false), // Not blocking
-            Ok(procfs::process::ProcState::Sleeping) => Ok(true), // Potentially blocked
-            Ok(procfs::process::ProcState::Waiting) => Ok(true),  // Definitely blocking
-            _ => Ok(true), // Other states like Stopped, Zombie, etc. could be considered blocked or non-killable
-        }
-    }
+    /// Gracefully stop a process with a timeout, falling back to SIGKILL if needed
+    pub async fn stop_process(pid: c_int, timeout_secs: u64, state: &mut AppState, state_path: &PathType) -> Result<(), Box<dyn std::error::Error>> {
+        log!(LogLevel::Trace, "Stopping process with PID: {} with a timeout of {} seconds", pid, timeout_secs);
 
-    /// Force kill a process using SIGKILL
-    pub fn force_kill_process(pid: c_int) -> Result<(), io::Error> {
-        unsafe {
-            if kill(pid, SIGKILL) != 0 {
-                return Err(io::Error::last_os_error());
-            }
-        }
-        Self::reap_zombie_process(pid);
-
-        Ok(())
-    }
-
-    /// Restart a process given a command and its arguments
-    pub fn restart_process(
-        pid: c_int,
-        command: &str,
-        args: &[&str],
-    ) -> Result<Child, Box<dyn std::error::Error>> {
-        // Kill the current process
-        Self::kill_process(pid)?;
-
-        // Wait for a moment to ensure the process is terminated
-        thread::sleep(Duration::from_secs(1));
-
-        // Start the process again
-        let child = Self::spawn_process(command, args, true)?;
-        Ok(child)
-    }
-
-    /// Send a specific signal to a process
-    pub fn send_signal(pid: c_int, signal: c_int) -> Result<(), Box<dyn std::error::Error>> {
-        unsafe {
-            if kill(pid, signal) != 0 {
-                return Err(io::Error::last_os_error().into());
-            }
-        }
-        Ok(())
-    }
-
-    /// Get the status of a running process by its PID
-    pub fn get_process_status(pid: c_int) -> Result<String, Box<dyn std::error::Error>> {
-        let process = procfs::process::Process::new(pid)?;
-        let stat = process.stat()?;
-        let status = match stat.state() {
-            Ok(procfs::process::ProcState::Running) => "Running",
-            Ok(procfs::process::ProcState::Sleeping) => "Sleeping",
-            Ok(procfs::process::ProcState::Zombie) => "Zombie",
-            Ok(procfs::process::ProcState::Stopped) => "Stopped",
-            Ok(procfs::process::ProcState::Dead) => "Dead",
-            _ => "Unknown",
-        };
-        Ok(status.to_string())
-    }
-
-    /// Gracefully stop a process with a timeout
-    pub fn stop_process(pid: c_int, timeout_secs: u64) -> Result<(), Box<dyn std::error::Error>> {
-        // Send SIGTERM to the process
         unsafe {
             if kill(pid, SIGTERM) != 0 {
                 return Err(io::Error::last_os_error().into());
@@ -145,12 +111,14 @@ impl ProcessManager {
         let start_time = Instant::now();
         while start_time.elapsed() < Duration::from_secs(timeout_secs) {
             if !Self::is_process_running(pid) {
+                log!(LogLevel::Trace, "Process with PID: {} has exited", pid);
                 return Ok(());
             }
             thread::sleep(Duration::from_millis(800));
         }
 
         // If the process did not exit, send SIGKILL
+        log!(LogLevel::Warn, "Process with PID: {} did not exit, sending SIGKILL", pid);
         unsafe {
             if kill(pid, SIGKILL) != 0 {
                 return Err(io::Error::last_os_error().into());
@@ -158,19 +126,15 @@ impl ProcessManager {
             Self::reap_zombie_process(pid);
         }
 
-        // Wait again to ensure the process is killed
-        let start_time = Instant::now();
-        while start_time.elapsed() < Duration::from_secs(2) {
-            if !Self::is_process_running(pid) {
-                return Ok(());
-            }
-            thread::sleep(Duration::from_millis(100));
-        }
+        log!(LogLevel::Trace, "Process with PID: {} has been forcefully terminated", pid);
+        state.data = String::from("Process forcefully terminated");
+        state.event_counter += 1;
+        update_state(state, state_path);
 
-        Err("Failed to kill the process after SIGKILL".into())
+        Ok(())
     }
 
-    /// Ensure that we poll the system to clean up the process if it's a zombie
+    /// Reap zombie processes to clean up system resources
     fn reap_zombie_process(pid: i32) {
         let _ = waitpid(Pid::from_raw(pid), None);
     }

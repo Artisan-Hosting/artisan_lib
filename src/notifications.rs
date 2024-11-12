353a58a6b7e6f950a8974ef7ab480d86b77695a0
Darrion Whitfield <dwhitfield@ramfield.net>
2024-11-11 19:46:27 -0600

    Fix: Update notification system

--- src/notifications.rs
+++ src/notifications.rs
@@ -1,12 +1,15 @@
 use colored::Colorize;
 use dusa_collection_utils::{
-    errors::{ErrorArrayItem, Errors}, log::LogLevel, log, stringy::Stringy
+    errors::{ErrorArrayItem, Errors, OkWarning, UnifiedResult, WarningArray, WarningArrayItem, Warnings},
+    log::LogLevel,
+    log,
+    stringy::Stringy,
 };
 use serde::{Deserialize, Serialize};
-use tokio::net::TcpStream;
 use std::fmt;
+use tokio::net::TcpStream;
 
-use crate::communication_proto::{send_message_tcp, Flags, ProtocolMessage, ProtocolStatus};
+use crate::communication_proto::{send_message, Flags, Proto, ProtocolStatus};
 
 const MAIL_ADDRESS: &str = "45.137.192.70:1827";
 
@@ -22,7 +25,12 @@ pub struct Email {
 // Display implementation for Email
 impl fmt::Display for Email {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Subject: {}, Body: {}", self.subject.bold().blue(), self.body.bold().blue())
+        write!(
+            f,
+            "Subject: {}, Body: {}",
+            self.subject.bold().blue(),
+            self.body.bold().blue()
+        )
     }
 }
 
@@ -48,32 +56,64 @@ impl Email {
     }
 
     /// Sends the email data over a TCP stream.
-    pub async fn send(&self, addr: Option<&str>) -> Result<(), ErrorArrayItem> {
+    #[rustfmt::skip]
+    pub async fn send(&self, addr: Option<&str>) -> UnifiedResult<OkWarning<()>> {
         if !self.is_valid() {
-            return Err(ErrorArrayItem::new(
+            return UnifiedResult::new(Err(ErrorArrayItem::new(
                 Errors::GeneralError,
                 "Invalid Email Data".to_owned(),
-            ));
+            )));
         }
 
-        let mut message: ProtocolMessage<String> = ProtocolMessage::new(Flags::OPTIMIZED, self.to_json()?)?;
-
-        let mut stream = match addr {
+        let stream_result: Result<TcpStream, UnifiedResult<OkWarning<()>>> = match addr {
             Some(addr) => TcpStream::connect(addr).await,
             None => TcpStream::connect(MAIL_ADDRESS).await,
-        }?;
+        }.map_err(|err| UnifiedResult::new(Err(ErrorArrayItem::from(err))));
 
-        log! {LogLevel::Trace, "Connected to: {:#?}", stream.peer_addr().unwrap()};
+        let mut stream = match stream_result {
+            Ok(stream) => stream,
+            Err(err) => return err,
+        };
 
-        match send_message_tcp(&mut stream, &mut message, true).await.map_err(|err| ErrorArrayItem::from(err)) {
-            Ok(status) => match status.expect(ProtocolStatus::OK) {
-                true => Ok(()),
-                false => {
-                    log!(LogLevel::Error, "Email failed to send ? {}", status);
-                    Ok(())
-                },
+        log!{LogLevel::Trace, "Connected to: {:#?}", stream.peer_addr().unwrap()};
+
+        let data_result: Result<String, UnifiedResult<OkWarning<()>>> = self.to_json()
+            .map_err(|err| UnifiedResult::new(Err(err)));
+
+        let data: String = match data_result {
+            Ok(data) => data,
+            Err(err) => return err,
+        };
+
+        match send_message::<TcpStream, String, ()>(
+            &mut stream, Flags::OPTIMIZED, data,
+            Proto::TCP, false
+        ).await {
+            Ok(response) => {
+                match response {
+                    Ok(response) => {
+                        let warning: WarningArrayItem = 
+                            WarningArrayItem::new_details(
+                            Warnings::UnexpectedBehavior, format!("{}", 
+                                    ProtocolStatus::from_bits_truncate(response.header.status))
+                            );
+
+                        return UnifiedResult::new(Ok(OkWarning{
+                            data: response.payload,
+                            warning: WarningArray::new(vec![warning]),
+                        }))
+                    },
+                    Err(error_code) => {
+                        let error: ErrorArrayItem = 
+                            ErrorArrayItem::new(Errors::Network, format!("{}", error_code));
+                        
+                        return UnifiedResult::new(Err(error))
+                    },
+                }
             },
-            Err(error) => Err(error),
+            Err(err) => {
+                return UnifiedResult::new(Err(ErrorArrayItem::from(err)))
+            },
         }
     }
 }

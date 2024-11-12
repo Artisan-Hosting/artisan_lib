3ceb60efcf199187fb492afa8a325c6afc1dab7f
Darrion Whitfield <dwhitfield@ramfield.net>
2024-11-11 19:46:48 -0600

    ADD: systemd api wrapper

---
+++ src/systemd.rs
@@ -0,0 +1,67 @@
+use std::error::Error;
+use std::{fmt, io};
+
+use dusa_collection_utils::stringy::Stringy;
+type ID = Stringy;
+
+/// Enum representing the possible statuses of a systemd service.
+#[derive(Debug)]
+pub enum ServiceStatus {
+    Active,
+    Inactive,
+    Failed,
+    Unknown,
+}
+
+impl fmt::Display for ServiceStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ServiceStatus::Active => write!(f, "Active"),
+            ServiceStatus::Inactive => write!(f, "Inactive"),
+            ServiceStatus::Failed => write!(f, "Failed"),
+            ServiceStatus::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+/// Wrapper struct for a systemd service, providing control functions.
+pub struct SystemdService {
+    service_name: ID,
+}
+
+impl SystemdService {
+    /// Creates a new `SystemdService` instance with the specified service name.
+    pub fn new(service_name: &str) -> io::Result<Self> {
+        match systemctl::exists(service_name)? {
+            true => {
+                Ok(Self {
+                    service_name: Stringy::Immutable(service_name.into()),
+                })
+            },
+            false => return Err(io::Error::new(io::ErrorKind::NotFound, format!{"{} not found", service_name})),
+        }
+    }
+
+    /// Start the service.
+    pub fn start(&self) -> Result<(), Box<dyn Error>> {
+        systemctl::start(&self.service_name)?;
+        Ok(())
+    }
+
+    /// Stop the service.
+    pub fn stop(&self) -> Result<(), Box<dyn Error>> {
+        systemctl::stop(&self.service_name)?;
+        Ok(())
+    }
+
+    /// Restart the service.
+    pub fn restart(&self) -> Result<(), Box<dyn Error>> {
+        systemctl::reload_or_restart(&self.service_name)?;
+        Ok(())
+    }
+
+    /// Check if the service is active.
+    pub fn is_active(&self) -> Result<bool, Box<dyn Error>> {
+        Ok(systemctl::is_active(&self.service_name)?)
+    }
+}

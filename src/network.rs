16a0ca4907376778152cacc537628a2e1ba48e53
Darrion Whitfield <dwhitfield@ramfield.net>
2024-12-20 15:09:18 -0600

    ADD: Dns resolving function to network.rs

--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,14 @@
+use std::error::Error;
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use dusa_collection_utils::errors::ErrorArrayItem;
+use dusa_collection_utils::log;
+use dusa_collection_utils::log::LogLevel;
 use get_if_addrs::get_if_addrs;
-
 use get_if_addrs::IfAddr;
+use trust_dns_resolver::{config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts}, AsyncResolver};
 
-const MAJOR_VERSION: &str = env!("CARGO_PKG_VERSION_MAJOR");
-const MINOR_VERSION: &str = env!("CARGO_PKG_VERSION_MINOR");
+use crate::version::aml_version;
 
 pub fn get_local_ip() -> Ipv4Addr {
     let if_addrs = match get_if_addrs() {
@@ -36,10 +38,51 @@ pub async fn get_external_ip() -> Result<IpAddr, ErrorArrayItem> {
     }
 }
 
-pub fn get_header_version() -> u8 {
-    
-    let major_int = MAJOR_VERSION.parse::<u8>().unwrap_or(0);
-    let minor_int = MINOR_VERSION.parse::<u8>().unwrap_or(0);
+pub async fn resolve_url(url: &str, resolver_addr: Option<IpAddr>) -> Result<Option<Vec<IpAddr>>, Box<dyn Error>> {
+    // Configure the resolver to use Cloudflare's DNS
 
-    ((major_int << 7) | (minor_int << 3) | 0).into() // Shifts and ignoring the patch number
+    let address: IpAddr = match resolver_addr {
+        Some(given) => match given {
+            IpAddr::V4(ipv4_addr) => IpAddr::from(ipv4_addr),
+            IpAddr::V6(ipv6_addr) => IpAddr::from(ipv6_addr),
+        },
+        None => IpAddr::from([1, 1, 1, 1]),
+    };
+
+    // * Don't actually test this with an Ipv6 address
+    let resolver = format!("{}:53", address);
+
+
+    let resolver_config = ResolverConfig::from_parts(
+        None, // Use the system domain
+        vec![], // No search list
+        vec![NameServerConfig {
+            socket_addr: resolver.parse()?,
+            protocol: Protocol::Udp,
+            tls_dns_name: None,
+            trust_nx_responses: true,
+            bind_addr: None,
+        }],
+    );
+    let resolver_opts = ResolverOpts::default();
+
+    // Create the resolver
+    let resolver = AsyncResolver::tokio(resolver_config, resolver_opts)?;
+
+    
+    match resolver.lookup_ip(url).await {
+        Ok(response) => {
+            let ips: Vec<_> = response.iter().collect();
+            return Ok(Some(ips))
+        },
+        Err(err) => {
+            log!(LogLevel::Error, "Failed to resolve {}: {}", url, err);
+            return Ok(None)
+        },
+    }
+}
+
+pub fn get_header_version() -> u16 {
+    let lib_version = aml_version();
+    lib_version.encode()
 }

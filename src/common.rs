c45a57aecbf9411f9e263ab0f40537930ea2c43f
Darrion Whitfield <dwhitfield@ramfield.net>
2025-01-26 22:15:54 -0600

    fix: sending status stored in the appstate instead of a generic running status

--- src/common.rs
+++ src/common.rs
@@ -26,7 +26,7 @@ pub async fn update_state(state: &mut AppState, path: &PathType, metrics: Option
                 app_id: state.config.app_name.clone(),
                 error: Some(state.error_log.clone()),
                 metrics,
-                status: Status::Running,
+                status: state.status,
                 timestamp: current_timestamp(),
             });
 
@@ -39,10 +39,10 @@ pub async fn update_state(state: &mut AppState, path: &PathType, metrics: Option
                             match payload {
                                 AppMessage::Response(command_response) => {
                                     if command_response.success {
-                                        log!(LogLevel::Trace, "State updated with aggregator !");
+                                        log!(LogLevel::Debug, "State updated with aggregator !");
                                     }
                                 },
-                                _ => log!(LogLevel::Warn, "Illegal response recieved while reporting status"),
+                                _ => log!(LogLevel::Debug, "Illegal response recieved while reporting status"),
                             }
                         },
                         Err(err) => {
@@ -57,7 +57,6 @@ pub async fn update_state(state: &mut AppState, path: &PathType, metrics: Option
     // saving the state info
     if let Err(err) = StatePersistence::save_state(state, path).await {
         log!(LogLevel::Error, "Failed to save state: {}", err);
-        state.is_active = false;
         state.error_log.push(ErrorArrayItem::new(
             Errors::GeneralError,
             format!("{}", err),
@@ -67,8 +66,8 @@ pub async fn update_state(state: &mut AppState, path: &PathType, metrics: Option
 
 // Update the state file in the case of a un handled error
 pub async fn wind_down_state(state: &mut AppState, state_path: &PathType) {
-    state.is_active = false;
     state.data = String::from("Terminated");
+    state.status = Status::Stopping;
     state.last_updated = current_timestamp();
     state.error_log.push(ErrorArrayItem::new(
         Errors::GeneralError,
